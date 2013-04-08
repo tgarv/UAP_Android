11bcd5a249bf896853b800902bc00c272e12ab1b
Thomas Garver <tgarv@mit.edu>
2013-04-07 21:51:11 -0400

    Messenger will now read an NFC tag and display the message written on it.

--- NFCMessenger/src/com/example/nfcmessenger/MainActivity.java
+++ NFCMessenger/src/com/example/nfcmessenger/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.nfcmessenger;
 
+import java.io.UnsupportedEncodingException;
+
 import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.Intent;
@@ -13,6 +15,7 @@ import android.os.Bundle;
 import android.os.Parcelable;
 import android.util.Log;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 	private NfcAdapter mAdapter;
@@ -47,6 +50,17 @@ public class MainActivity extends Activity {
 
         // Setup a tech list for all NfcF tags
         mTechLists = new String[][] { new String[] { NfcF.class.getName() } };
+        
+        // Now, check if the application was launched by an Android Application Record (AAR), which means that it
+        // was launched by a tag and we should call onNewIntent to force the app to behave like it does when it's
+        // already open when a tag is read.
+        Bundle extras = getIntent().getExtras();
+        if (extras != null){
+        	if (extras.containsKey("android.nfc.extra.TAG")) {
+        		// This means the app was launched by an AAR, so we should process the intent like we would when resuming.
+        		onNewIntent(getIntent());
+        	}
+        }
     }
 
     @Override
@@ -60,9 +74,7 @@ public class MainActivity extends Activity {
     public void onResume() {
         super.onResume();
         if (mAdapter != null){
-        	mAdapter.enableForegroundDispatch(this, mPendingIntent, mFilters,
-                mTechLists);
-        	Log.i("NFC", "HERE!");
+        	mAdapter.enableForegroundDispatch(this, mPendingIntent, mFilters, mTechLists);
         }
     }
 
@@ -79,6 +91,15 @@ public class MainActivity extends Activity {
         	for(Parcelable parcel : p){
         		NdefMessage ndef = (NdefMessage) parcel;
         		Log.i("NFC", "NDEF message: " + ndef.toString());
+        		String payload = "";
+        		try {
+					payload = new String(ndef.getRecords()[0].getPayload(), 0, ndef.getRecords()[0].getPayload().length, "ASCII");
+				} catch (UnsupportedEncodingException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+        		Log.i("NFC", "Message payload: " + payload);
+        		((TextView)findViewById(R.id.textView1)).setText(payload);
         	}
         }
         else
